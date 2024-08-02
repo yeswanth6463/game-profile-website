2e066a1807cb3d8589a1594820cd8a1fc5c2c9a1
YESWANTH <113580288+yeswanth6463@users.noreply.github.com>
2024-08-02 19:05:59 +0530

    commit

---
+++ full_website/script.js
@@ -0,0 +1,8 @@
+const sidebar = document.querySelector('.sidebar');
+
+const togglebtn = document.querySelector('.toggle-btn');
+
+togglebtn.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+})
+
